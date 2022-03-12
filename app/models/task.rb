5b8dbc492de0cb1422d13f0f19ed305683f9aa25
Nif-Uchi <hirex0609@gmail.com>
2022-03-12 16:14:23 +0900

    task,labemodelとtableにvalidationを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  validates :task_name, presence: true, length: { maximum: 255 }
+  validates :priority, presence: true,  numericality: { only_integer: true}
+  validates :status, presence: true,  numericality: { only_integer: true}
 end
