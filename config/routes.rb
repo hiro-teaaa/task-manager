d1a3d9a032bf799edc9982344029db2068b267a5
Nif-Uchi <hirex0609@gmail.com>
2022-03-20 16:32:00 +0900

    ログアウトボタンの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :tasks, :task_labels, :labels
   root "tasks#index"
   get 'tasks/new', :to => 'tasks#new'
